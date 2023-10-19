6ea80cf58f126fd12ae853da8f2106ec8873b54e
Aman singh <amansingh162325@gmail.com>
2023-10-20 00:53:29 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,47 @@
+const reviews = [
+    {
+      id: 1,
+      name: "Aman Singh Rajput",
+      job:  "Full stack Web developer",
+      image: "https://media.licdn.com/dms/image/D4D03AQFpaKSW1sNt_A/profile-displayphoto-shrink_800_800/0/1687896829282?e=1700092800&v=beta&t=ZJJi8vre487b-iwjV7dsAVRime6DVNO5_9iyUqtCgos",
+      text: "|| Intershala verify condidate 5-⭐ || ✨Codechef 2-⭐ || ✨problem solving c/c++, Java ✨|| ✨web developer HTMl/CSS/JAVA SCRIPT and NODE.JS✨ || ✨ Graphic Design ✨|| ✨DSA✨ ||✨I E T Agra, CSE24 ✨",
+   },
+    
+    {
+        id: 2,
+        name: "Ashish Mishra",
+        job:  "Full stack Web developer",
+        image: "https://media.licdn.com/dms/image/D4D03AQFd_K_B89xvrQ/profile-displayphoto-shrink_200_200/0/1695801589456?e=1703116800&v=beta&t=e5SfuOhHVEJKKZmms6KUcZ3RQxahu2CSBJ-o6kqX4RM",
+        text: "Ex : Intern @Tata Motors | GSSOC'23 Contributor | Web Developer | Subject Matter Expert | Problem Solver - C/C++ | Teaching Enthusiastic | 800+ problem solved at Leetcode✨CodeChef✨GFG✨Coding Ninjas",
+    },
+    {
+        id: 3,
+        name: "Abhishek Gupta",
+        job:  "Machine learning",
+        image: "https://media.licdn.com/dms/image/C4E03AQG36J9PndtBzQ/profile-displayphoto-shrink_200_200/0/1646668300909?e=1700092800&v=beta&t=o-Qrl0qbxQVpTFhAviobKrZB8ga3xvLkQvzjFo8uRlQ",
+        text: "Enthusiastic Learner | Programmer | C/C++ | Python | React | DevOps | Cloud | Hactoberfest'22 🎉💫",
+    },
+    {
+        id: 4,
+        name: "Surya Mani Verma ",
+        job:  "Machine learning",
+        image: "https://media.licdn.com/dms/image/D4D03AQFUbkgsNuAUjQ/profile-displayphoto-shrink_200_200/0/1684346587706?e=1700092800&v=beta&t=GCmMPFazORJOZTVxkwvFlJtJyxhXPWko9JAMBCo0IeI",
+        text: "Codechef 2⭐ (C++)|| C/C++(Hacker rank 3⭐)|| Python || IET'24 B.E(CSE)",
+    },
+    {
+        id: 5,
+        name: "Saket Singh ",
+        job:  "Full stack Web developer",
+        image: "https://media.licdn.com/dms/image/D4D03AQGp1fp4I8EHoQ/profile-displayphoto-shrink_200_200/0/1676969489109?e=1700092800&v=beta&t=9pQYirtcLnrpM3qVyAsDeN8CHuSXTlqp_XUa3mOT86A",
+        text: "intern@Oasis Infobyte |Learning MERN stack @CodeHelp | B.tech 3rd year Student at Institute of Engineering and Technology (IET) agra",
+    },
+    {
+      id: 6,
+      name: "Shubhanshu Rao",
+      job: "Software Engineer",
+      image: "https://media.licdn.com/dms/image/D5635AQGfVUDVlC0r-w/profile-framedphoto-shrink_200_200/0/1690596835662?e=1698350400&v=beta&t=kn956m27wuKKBncoj9VGHXLVvc-vsBprqCfuFzzrrqs",
+      text: "Intern @The Sparks Foundation | GSSOC`23 | SSOC'23 | DSA | Web developer | Problem Solving | C++/C",
+    },
+  ];
+  
+  export default reviews;
